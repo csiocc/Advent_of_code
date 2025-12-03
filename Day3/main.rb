844aff5c6fbae2d2f5ba2535c677c64baaa884f0
Christian <christian.siegrist@open-circle.ch>
2025-12-03 12:16:42 +0100

    try fix but doesnt work

--- Day3/main.rb
+++ Day3/main.rb
@@ -2,7 +2,7 @@ class PuzzleSolver
   def initialize
     @input = read_input('input.txt')
     @result = []
-    @clicks = 0
+    @line = 0
     @result2 = 0
   end
   DEBUG = false
@@ -19,13 +19,17 @@ class PuzzleSolver
 
   def loop_through(value)
     value.each do |line|
-      @clicks += 1
+      @line += 1
       highest, second_highest = find_highest(line)
       p "highest: #{highest[:n]}, second highest: #{second_highest[:n]}" if DEBUG
-      no = ("#{highest[:n]}#{second_highest[:n]}").to_i if highest[:i] < second_highest[:i]
-      no = ("#{second_highest[:n]}#{highest[:n]}").to_i if highest[:i] > second_highest[:i]
-      @result << no
-      p "pushing #{no} to result array"
+      no = []
+      no[highest[:i]] = highest[:n]
+      no[second_highest[:i]] = second_highest[:n]
+      no.compact!
+      p "no = #{no}"
+      no2 = no.join.to_i
+      @result << no2
+      p "pushing #{no2} to result array"
     end
     p "result: #{@result.sum}" 
   end
@@ -43,19 +47,35 @@ class PuzzleSolver
       end
     end
     second_highest = find_second(value, highest[:i])
-    p "found highest: #{highest}, second highest: #{second_highest}"
+    p "found highest: #{highest}, second highest: #{second_highest} line: #{@line}" 
     return highest, second_highest
   end
 
   def find_second(value, i)
-    second_highest = {n: 0, i: 0}
+    second_highest = {n: -1, i: nil}
+
     value.each_with_index do |n, ii|
-      next if ii == i
+      next unless ii > i
+    
       current = n.to_i
 
-      if current > second_highest[:n]
-        second_highest[:n] = current.to_i
+      if current >= second_highest[:n]
+        second_highest[:n] = current
         second_highest[:i] = ii
+      end
+    end
+
+    if second_highest[:n].nil?
+      value.each_with_index do |n, ii|
+        next if ii <= i
+
+        current = n.to_i
+        
+        if current >= second_highest[:n]
+          second_highest[:n] = current
+          second_highest[:i] = ii
+        end
+
       end
     end
     return second_highest
